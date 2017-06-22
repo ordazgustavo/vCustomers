58dd54cfec53ea7a5676665acba4ca12552ea7d2
Gustavo Ordaz <ordazsgustavo@gmail.com>
2017-06-22 16:09:53 -0400

    first commit

---
+++ src/components/CustomerDetails.vue
@@ -0,0 +1,51 @@
+<template lang="html">
+   <div class="details container">
+      <router-link to="/">Back</router-link>
+      <h1 class="page-header">{{customer.first_name}} {{customer.last_name}}
+         <span class="pull-right">
+            <router-link class="btn btn-primary" v-bind:to="'/edit/'+customer.id">Edit</router-link>
+            <button class="btn btn-danger" v-on:click="deleteCustomer(customer.id)">Delete</button>
+         </span>
+      </h1>
+      <ul class="list-group">
+         <li class="list-group-item"><span class="glyphicon glyphicon-phone" aria-hidden="true"></span> {{customer.phone}}</li>
+         <li class="list-group-item"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> {{customer.email}}</li>
+      </ul>
+      <ul class="list-group">
+         <li class="list-group-item"> {{ customer.address}}</li>
+         <li class="list-group-item"> {{ customer.city}}</li>
+         <li class="list-group-item"> {{ customer.state}}</li>
+      </ul>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'customerdetails',
+   data() {
+      return {
+         customer: ''
+      }
+   },
+   methods: {
+      fetchCustomer(id) {
+         this.$http.get('http://slimapp/api/customer/'+id)
+            .then(function(res) {
+               this.customer = res.body;
+            });
+      },
+      deleteCustomer(id) {
+         this.$http.delete('http://slimapp/api/customer/delete/'+id)
+            .then(function(res) {
+               this.$router.push({path: '/', query: {alert: 'Customer Deleted'}})
+            });
+      }
+   },
+   created: function() {
+      this.fetchCustomer(this.$route.params.id);
+   }
+}
+</script>
+
+<style lang="css">
+</style>
